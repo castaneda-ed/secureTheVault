721ec6341e89cfae6d13b1d4798cc529c7042986
Edwin <edwincastaneda0801@gmail.com>
2024-09-04 15:36:09 -0500

    done-with-vault

---
+++ secure_vault.js
@@ -0,0 +1,13 @@
+// I am going to create three variables
+// I am going to set the values of the strings to mathematical operations
+//Then use the browser's console to view the output
+//Then use the alert pop up to display the vault's combination
+
+const firstNumber = 5 + 5;
+const secondNumber = 5 * 8;
+const thirdNumber = 50 - 11;
+const text = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:';
+alert(text)
+alert(firstNumber)
+alert(secondNumber)
+alert(thirdNumber)
